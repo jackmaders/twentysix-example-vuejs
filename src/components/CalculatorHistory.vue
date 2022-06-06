815a5bd2fdc53d3ba79aa5ec0daf4f8690b7568f
Jack Maders <jackwmaders@gmail.com>
2022-06-06 21:29:12 +0100

    initial setup of vue and tailwind

---
+++ src/components/CalculatorHistory.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-slate-500 h-10 w-10"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/TheCalculator.vue
@@ -0,0 +1,16 @@
+<template>
+  <calculator-history />
+  <calculator-display />
+  <calculator-controls />
+</template>
+
+<script>
+import CalculatorControls from "./CalculatorControls.vue";
+import CalculatorDisplay from "./CalculatorDisplay.vue";
+import CalculatorHistory from "./CalculatorHistory.vue";
+export default {
+  components: { CalculatorDisplay, CalculatorControls, CalculatorHistory },
+};
+</script>
+
+<style></style>
